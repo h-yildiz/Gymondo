4688b3689d70277f899c7d2ce33fb1090655e1c0
h-yildiz <68542303+h-yildiz@users.noreply.github.com>
2022-06-27 14:53:08 +0200

    first commit

---
+++ src/test/java/com/Gymondo/utilities/BrowserUtils.java
@@ -0,0 +1,39 @@
+package com.Gymondo.utilities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BrowserUtils {
+
+    /**
+     * returns a list of string from a list of elements
+     *
+     * @param list of web elements
+     * @return list of string
+     */
+    public static List<String> getElementsText(List<WebElement> list) {
+        List<String> elemTexts = new ArrayList<>();
+        for (WebElement el : list) {
+            elemTexts.add(el.getText());
+        }
+        return elemTexts;
+    }
+
+
+    /**
+     * Waits for the provided element to be visible on the page
+     *
+     * @param element
+     * @param timeToWaitInSectime
+     * @return WebElement
+     */
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
+        WebDriverWait wait = new WebDriverWait(Driver.get(), timeToWaitInSec);
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+}
